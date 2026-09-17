a6967113f6356495a18be0d38f39dcb486da7ac8
bethrmz13 <alissonparedes83@gmail.com>
2026-09-17 14:12:30 -0500

    feat(AuthService): Creación de DTO para Registro Activo (datos clave), RegistroActivoDTO

---
+++ AuthService/DTOs/RegistroActivoDto.cs
@@ -0,0 +1,10 @@
+namespace AuthService.DTOs
+{
+    public class RegistroActivoDto
+    {
+        public string Tipo { get; set;}
+        public string Detalle { get; set;}
+        public decimal Precio { get; set;}
+        public DateTime FechaIngreso { get; set;}
+    }
+}
