6479d29d15d8d4a8e7ccba5bb10c31536c959efe
bethrmz13 <alissonparedes83@gmail.com>
2026-09-21 12:22:20 -0500

    fix(Frontend): ajusta categorias del formulario para coincidir con BD

--- ActivosService/Controllers/ActivosController.cs
+++ ActivosService/Controllers/ActivosController.cs
@@ -23,12 +23,12 @@ namespace ActivosService.Controllers
             Console.WriteLine($"\n--- NUEVO ACTIVO AÑADIDO ---");
             Console.WriteLine($"Tipo: {activoDto.Tipo}");
             Console.WriteLine($"Fecha de Compra: {activoDto.FechaIngreso}");
-            Console.WriteLine($"Fecha de Corte: {activoDto.FechaCorte}\n");
+            
             
             using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
 
             var sql = @"
-                INSERT INTO auth.Activos (Tipo, Detalle, Precio, FechaIngreso) VALUES
+                INSERT INTO dbo.Activos (Tipo, Detalle, Precio, FechaIngreso) VALUES
                 (@Tipo, @Detalle, @Precio, @FechaIngreso)";
 
             var filasAfectadas = connection.Execute(sql, activoDto);
